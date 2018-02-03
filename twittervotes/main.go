2541e06240b43362cb7fb6c39ca364c964fd44c9
鈴木康文 <uni0360@gmail.com>
2018-02-04 06:15:00 +0900

    make readFromTwitter

--- twittervotes/main.go
+++ twittervotes/main.go
@@ -5,3 +5,33 @@ import "fmt"
 func main() {
 	fmt.Println("vim-go")
 }
+
+var db *mgo.Session
+
+func dialdb() error {
+	var err error
+	log.Println("MongDBにダイヤル中: loaclhost")
+	db, err = mgo.Dial("localhost")
+	return err
+}
+
+func closedb() {
+	db.Close()
+	log.Println("データベース接続が閉じられました")
+}
+
+type poll struct {
+	Options []string
+}
+
+func loadOptions() ([]string, error) {
+	var options []string
+	iter := db.DB("ballots").C("polls").Find(nil).Iter()
+
+	var p poll
+	for iter.Next(&p) {
+		options = append(options, p.Options...)
+	}
+	iter.Close()
+	return options, iter.Err()
+}
--- twittervotes/twitter.go
+++ twittervotes/twitter.go
@@ -62,3 +62,41 @@ func setupTwitterAuth() {
 		},
 	}
 }
+
+var (
+	authSetupOnce sync.Once
+	httpClient    *http.Client
+)
+
+func makeRequest(req *http.Request, params url.Values) (*http.Response, error) {
+	authSetupOnce.Do(func() {
+		setupTwitterAuth()
+		httpClient = &http.Client{
+			Transport: &http.Transport{
+				Dial: dial,
+			},
+		}
+	})
+	formEnc := params.Encode()
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Content-Length", strconv.Itoa(len(formEnc)))
+	req.Header.Set("Authorization",
+		authClient.AuthorizationHeader(creds, "POST", req.URL, params))
+	return httpClient.Do(req)
+}
+
+type tweet struct {
+	Text string
+}
+
+func readFromTwitter(votes chan<- string) {
+	options, err := loadOptions()
+	if err != nil {
+		log.Println("選択肢の読み込みに失敗しました:". err)
+		return
+	}
+	u, err := nil {
+		log.Println("URLの解析に失敗しました:", err)
+		return
+	}
+}
